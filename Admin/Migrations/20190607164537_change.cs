1ce7ceec0a7e53a9fb4de658b729e2e92fb035f3
hernan saltiveri <hernansaltiveri@gmail.com>
2019-06-07 14:38:41 -0300

    changes

---
+++ Admin/Migrations/20190607164537_change.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Admin.Migrations
+{
+    public partial class change : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Sensors",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Sensors");
+        }
+    }
+}
--- Admin/Models/Sensor.cs
+++ Admin/Models/Sensor.cs
@@ -10,6 +10,9 @@ namespace Admin.Models
         public int SensorId { get; set; }
         public string sensorSerialNumber { get; set; }
 
+        //Could measure one or multiple values. In the last case the type value should have the value multiple
+        public string Type { get; set; }
+
         public List<Measurement> Measurements { get; set; }
         public virtual Device Device { get; set; }
 
